530822a0b26e2941391b6bd7c434504e026bb1ae
Javier Rojas <javierrojas14.jr@gmail.com>
2024-01-09 14:11:45 -0300

    Agrega archivos iniciales

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A script to automate the 3D analysis of the APBS potential for molecular dynamics',
+    author='Javier Rojas',
+    license='MIT',
+)
